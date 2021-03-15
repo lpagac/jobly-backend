3f1e50dd22241cd214343b0d556d63965ac2d2a0
Lucas Pagac <lucaspagac0@gmail.com>
2021-03-15 13:00:14 -0700

    adding patch to cors

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const app = express();
 
 const corsOptions = {
   "origin": "*",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   "preflightContinue": false,
   "optionsSuccessStatus": 204
 }
