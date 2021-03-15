2782ec98b822c84161723cdddf2fa869dc104bb2
Lucas Pagac <lucaspagac0@gmail.com>
2021-03-15 12:56:05 -0700

    adding PATCH to cors

--- app.js
+++ app.js
@@ -17,7 +17,14 @@ const morgan = require("morgan");
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
